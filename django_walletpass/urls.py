a9c29148d9ebada898ca61c7e27be89c00ab95fa
Nache <nache.nache@gmail.com>
2023-01-09 13:19:51 +0100

    deprecated django.conf.urls.url -> use re_path

--- django_walletpass/urls.py
+++ django_walletpass/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import classviews
 
 urlpatterns = [
-    url(
+    re_path(
         r'^v1/devices/(?P<device_library_id>.+)/registrations/(?P<pass_type_id>.+)/(?P<serial_number>.+)$',
         classviews.RegisterPassViewSet.as_view({
             'post': 'create',
@@ -10,21 +10,21 @@ urlpatterns = [
         }),
         name='walletpass_register_pass',
     ),
-    url(
+    re_path(
         r'^v1/devices/(?P<device_library_id>.+)/registrations/(?P<pass_type_id>.+)$',
         classviews.RegistrationsViewSet.as_view({
             'get': 'list',
         }),
         name='walletpass_registrations',
     ),
-    url(
+    re_path(
         r'^v1/passes/(?P<pass_type_id>.+)/(?P<serial_number>.+)$',
         classviews.LatestVersionViewSet.as_view({
             'get': 'retrieve'
         }),
         name='walletpass_latest_version',
     ),
-    url(
+    re_path(
         r'^v1/log$',
         classviews.LogViewSet.as_view({
             'post': 'create',
