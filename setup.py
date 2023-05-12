baed2bf78485a07c32b78402662a2caacf4a4360
Jirka Schaefer <jirka@tschitschereengreen.com>
2023-05-12 22:03:28 +0200

    add required lib to requirements

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         'Django>=2.0',
         'cryptography>=2.4.2',
-        'apns2>=0.7.1',
+        'aioapns~=2.2',
         'pyopenssl',
         'djangorestframework>=3.8',
     ],
