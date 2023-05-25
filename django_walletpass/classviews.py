6ea752ba69129ff82b699db898cfa9ef6e7f3dbe
Jirka Schaefer <jirka@tschitschereengreen.com>
2023-05-25 17:19:14 +0200

    Revert "linter love 4"
    This reverts commit 5afda04f8da33f8c9a6d0d707a14e5396e785501.

--- django_walletpass/classviews.py
+++ django_walletpass/classviews.py
@@ -10,31 +10,28 @@ from django.http import HttpResponse
 from django.middleware.http import ConditionalGetMiddleware
 from django.shortcuts import get_object_or_404
 from django.utils.http import http_date
-from django_walletpass.models import Log, Pass, Registration
-from django_walletpass.settings import dwpconfig as WALLETPASS_CONF
 from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django_walletpass.models import Log, Pass, Registration
+from django_walletpass.settings import dwpconfig as WALLETPASS_CONF
 
 # legacy constant, remove when it can be assumed no timestamps of this format are out
 # there anymore
-FORMAT = "%Y-%m-%d %H:%M:%S"
+FORMAT = '%Y-%m-%d %H:%M:%S'
 PASS_REGISTERED = django.dispatch.Signal()
 PASS_UNREGISTERED = django.dispatch.Signal()
 
 
 def get_pass(pass_type_id, serial_number):
-    return get_object_or_404(
-        Pass, pass_type_identifier=pass_type_id, serial_number=serial_number
-    )
+    return get_object_or_404(Pass, pass_type_identifier=pass_type_id, serial_number=serial_number)
 
 
 class RegistrationsViewSet(viewsets.ViewSet):
     """
     Gets the Serial Numbers for Passes Associated with a Device
     """
-
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny, )
 
     def list(self, request, device_library_id, pass_type_id):
         passes = Pass.objects.filter(
@@ -44,25 +41,20 @@ class RegistrationsViewSet(viewsets.ViewSet):
         if passes.count() == 0:
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-        if "passesUpdatedSince" in request.GET:
+        if 'passesUpdatedSince' in request.GET:
             try:
                 # must be able to read UTC isoformat with TZ and FORMAT datetime string
                 # as well
-                date = parse(request.GET["passesUpdatedSince"])
+                date = parse(request.GET['passesUpdatedSince'])
                 passes = passes.filter(updated_at__gt=date)
             except NonExistentTimeError:
                 date = date.replace(hour=0, minute=0)
                 passes = passes.filter(updated_at__gt=date)
 
         if passes:
-            last_updated = passes.aggregate(Max("updated_at"))["updated_at__max"]
-            serial_numbers = [
-                p.serial_number for p in passes.filter(updated_at=last_updated).all()
-            ]
-            response_data = {
-                "lastUpdated": last_updated.isoformat(),
-                "serialNumbers": serial_numbers,
-            }
+            last_updated = passes.aggregate(Max('updated_at'))['updated_at__max']
+            serial_numbers = [p.serial_number for p in passes.filter(updated_at=last_updated).all()]
+            response_data = {'lastUpdated': last_updated.isoformat(), 'serialNumbers': serial_numbers}
             return Response(response_data)
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
@@ -76,26 +68,23 @@ class RegisterPassViewSet(viewsets.ViewSet):
     get: Registers a Device to Receive Push Notifications for a Pass
     destroy: Unregister a Device
     """
-
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny, )
 
     def create(self, request, device_library_id, pass_type_id, serial_number):
 
         pass_ = get_pass(pass_type_id, serial_number)
-        if (
-            request.META.get("HTTP_AUTHORIZATION")
-            != f"ApplePass {pass_.authentication_token}"
-        ):
+        if request.META.get('HTTP_AUTHORIZATION') != f"ApplePass {pass_.authentication_token}":
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
         registration = Registration.objects.filter(
-            device_library_identifier=device_library_id, pazz=pass_,
+            device_library_identifier=device_library_id,
+            pazz=pass_,
         )
         if registration:
             return Response({}, status=status.HTTP_200_OK)
         body = json.loads(request.body)
         new_registration = Registration(
             device_library_identifier=device_library_id,
-            push_token=body["pushToken"],
+            push_token=body['pushToken'],
             pazz=pass_,
         )
         new_registration.save()
@@ -104,13 +93,11 @@ class RegisterPassViewSet(viewsets.ViewSet):
 
     def destroy(self, request, device_library_id, pass_type_id, serial_number):
         pass_ = get_pass(pass_type_id, serial_number)
-        if (
-            request.META.get("HTTP_AUTHORIZATION")
-            != f"ApplePass {pass_.authentication_token}"
-        ):
+        if request.META.get('HTTP_AUTHORIZATION') != f"ApplePass {pass_.authentication_token}":
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
         registration = Registration.objects.filter(
-            device_library_identifier=device_library_id, pazz=pass_,
+            device_library_identifier=device_library_id,
+            pazz=pass_,
         )
         if registration:
             return Response({}, status=status.HTTP_200_OK)
@@ -123,31 +110,23 @@ class LatestVersionViewSet(viewsets.ViewSet):
     """
     get: Gets the latest version of a Pass
     """
-
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny, )
 
     def retrieve(self, request, pass_type_id, serial_number):
         pass_ = get_pass(pass_type_id, serial_number)
 
-        if (
-            request.META.get("HTTP_AUTHORIZATION")
-            != f"ApplePass {pass_.authentication_token}"
-        ):
+        if request.META.get('HTTP_AUTHORIZATION') != f"ApplePass {pass_.authentication_token}":
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
-        if WALLETPASS_CONF["STORAGE_HTTP_REDIRECT"]:
-            return Response(
-                {}, status=status.HTTP_302_FOUND, headers={"Location": pass_.data.url}
-            )
+        if WALLETPASS_CONF['STORAGE_HTTP_REDIRECT']:
+            return Response({}, status=status.HTTP_302_FOUND, headers={'Location': pass_.data.url})
 
-        response = HttpResponse(
-            pass_.data.read(), content_type="application/vnd.apple.pkpass"
-        )
-        response["Content-Disposition"] = "attachment; filename=pass.pkpass"
+        response = HttpResponse(pass_.data.read(), content_type='application/vnd.apple.pkpass')
+        response['Content-Disposition'] = 'attachment; filename=pass.pkpass'
 
-        response["Last-Modified"] = http_date(timegm(pass_.updated_at.utctimetuple()))
+        response['Last-Modified'] = http_date(timegm(pass_.updated_at.utctimetuple()))
 
-        def _get_response(request):  # noqa: W0613
+        def _get_response(request):  #noqa: W0613
             return response
 
         return ConditionalGetMiddleware(get_response=_get_response)(request)
@@ -158,11 +137,10 @@ class LogViewSet(viewsets.ViewSet):
     """
     Logs messages from devices
     """
-
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny, )
 
     def create(self, request):
         json_body = json.loads(request.body)
-        for message in json_body["logs"]:
+        for message in json_body['logs']:
             Log(message=message).save()
         return Response({}, status=status.HTTP_200_OK)
