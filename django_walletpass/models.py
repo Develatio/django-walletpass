020428ce4782ce69baabe36b1e1bf08c0ffd004e
Artūrs Cirsis <arturs.cirsis@gmail.com>
2023-07-09 22:03:14 +0300

    parse_log Class method

--- django_walletpass/models.py
+++ django_walletpass/models.py
@@ -332,3 +332,53 @@ class Log(models.Model):
 
     def __str__(self):
         return self.message
+
+    @classmethod
+    def parse_log(cls, log, message):
+        pattern_register = r"\[(.*?)\]\s(.*?)\s\(for device (.*?), pass type (.*?), serial number (.*?); with web service url (.*?)\)\s(.*?): (.*$)"
+        pattern_get = r"\[(.*?)\]\s(.*?)\s\(pass type (.*?), serial number (.*?), if-modified-since \(.*?\); with web service url (.*?)\) (.*?): (.*$)"
+        pattern_web_service_error = r"\[(.*?)\]\s(.*?)\sfor (.*?)\s\((.*?)\):\s(.*)"
+
+        match_register = re.match(pattern_register, message)
+        match_get = re.match(pattern_get, message)
+        match_web_service_error = re.match(pattern_web_service_error, message)
+
+        if match_register:
+            timestamp_str, task_type, device_id, pass_type_identifier, serial_number, web_service_url, status, msg = match_register.groups()
+        elif match_get:
+            timestamp_str, task_type, pass_type_identifier, serial_number, web_service_url, status, msg = match_get.groups()
+            device_id = None  # 'Get pass task' entries don't include device_id
+        elif match_web_service_error:
+            timestamp_str, task_type, pass_type_identifier, web_service_url, msg = match_web_service_error.groups()
+            serial_number = None
+            device_id = None
+            status = "error"
+        else:
+            log.status = 'unknown'
+            log.message = message
+            log.save()
+            return  # Log entry didn't match any known pattern
+
+        if 'error' in status:
+            status = 'error'
+        elif 'warning' in status:
+            status = 'warning'
+
+        log.created_at = datetime.datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S %z")
+        log.status = status
+        log.task_type = task_type
+        log.device_id = device_id
+        log.pass_type_identifier = pass_type_identifier
+        log.serial_number = serial_number
+        log.web_service_url = web_service_url
+        log.msg = msg
+        log.message = message
+
+        if serial_number:
+            try:
+                pazz = Pass.objects.get(serial_number=serial_number)
+                log.pazz = pazz
+            except Pass.DoesNotExist:
+                pass
+
+        log.save()
